01ca09b33922fb7310a0ec5f8c4948b4a0ed427a
moviePush <38831750+moviePush@users.noreply.github.com>
2022-12-02 00:51:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,23 +7,26 @@
  */
 const USER_CONFIG = {
 
+  //USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx0a1593d70a9f1633',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'e276958d9e0d10ae2824180abf7dd8db',
 
   PROVINCE: '广东',
   CITY: '惠州',
+  
+
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '初雪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-gq55pEg8zu7wpignuS8lqZfQY0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'qixrmU161hDdMx9pTAPFv-awk8M8vpPZbuyTSjxuRy0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -42,7 +45,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-08-16' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,13 +54,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '325-SUn1u2LvuD2nbNw16HkqDO_rNL2Eja4ngYbwYsY',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '时值',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-gq55oqlAuJncIo9ZKgqUUT6uCE',
     }
   ],
 
