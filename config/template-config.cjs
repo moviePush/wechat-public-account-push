8dc938aba072ff84660fdc30561757208c1a5536
moviePush <38831750+moviePush@users.noreply.github.com>
2022-12-08 01:07:50 +0800

    Update template-config.cjs
    修改模板，只有喝水提醒

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,38 +1,10 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
-    id: 'sDIh-6uF21ZVDcSdiNsdgt4yl3pyenp5LcH3nPd_mBA',
-    title: '亲爱的, 早上好',
+    id: 'TpHjp9ZqWlQZH02cfaP7TDA9lMxtCHMXfy7TlY9Ep0Y',
+    title: '喝水小助手',
     desc: `
-      **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      崔老师,{{remind.DATA}}
     `
   },
   {
