98cfa52ced773d19cd9c2ab6b039f9066b7a7434
moviePush <38831750+moviePush@users.noreply.github.com>
2022-12-08 01:08:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-gq55pEg8zu7wpignuS8lqZfQY0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'sDIh-6uF21ZVDcSdiNsdgt4yl3pyenp5LcH3nPd_mBA',
+      useTemplateId: 'TpHjp9ZqWlQZH02cfaP7TDA9lMxtCHMXfy7TlY9Ep0Y',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
